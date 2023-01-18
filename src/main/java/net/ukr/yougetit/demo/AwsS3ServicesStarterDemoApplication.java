b10716da359a944994172e75e985cdc7b4988648
Kostyantyn Panchenko <yougetit@ukr.net>
2023-01-18 18:11:39 +0200

    Demonstrate usage of all methods

--- src/main/java/net/ukr/yougetit/demo/AwsS3ServicesStarterDemoApplication.java
+++ src/main/java/net/ukr/yougetit/demo/AwsS3ServicesStarterDemoApplication.java
@@ -25,6 +25,25 @@ public class AwsS3ServicesStarterDemoApplication {
       log.info("Uploading build.gradle...");
       awsS3Service.upload("aws-s3-services-starter", Paths.get("build.gradle"));
       log.info("File uploaded.");
+
+      log.info("Downloading build.gradle...");
+      awsS3Service.download("aws-s3-services-starter", "build.gradle", "build");
+      log.info("File downloaded.");
+
+      log.info("Making a copy of build.gradle...");
+      awsS3Service.copy("aws-s3-services-starter", "build.gradle", "recycle-this");
+      log.info("Copied.");
+
+      System.out.println("In aws-s3-services-starter bucket: " + awsS3Service.listFiles("aws-s3-services-starter"));
+      System.out.println("In recycle-this bucket: " + awsS3Service.listFiles("recycle-this"));
+
+      log.info("Deleting build.gradle...");
+      awsS3Service.delete("aws-s3-services-starter", "build.gradle");
+      awsS3Service.delete("recycle-this", "build.gradle");
+      log.info("Deleted.");
+
+      System.out.println("In aws-s3-services-starter bucket: " + awsS3Service.listFiles("aws-s3-services-starter"));
+      System.out.println("In recycle-this bucket: " + awsS3Service.listFiles("recycle-this"));
     };
   }
 }
